35832c97a09ec404a4876c542708f8f03a1a1d58
AB <andybowman300@gmail.com>
2026-03-23 13:03:24 -0600

    add ascii val checks and read raw inp

--- src/pi/main.py
+++ src/pi/main.py
@@ -1,7 +1,35 @@
 import argparse
+import sys
 from motor import MotorController
 from drive import Drive
 
+# # NOTE
+#     #linux/mac 
+#         tty, terminos
+#     FOR WINDOWS CHECK (I use windows to debug so we can remove all os checks once base working on the pi)
+#         msvcrt.getwch()
+
+if sys.platform == "win32":
+    import msvcrt
+    def getKey():
+        key = msvcrt.getwch()
+        #win arrow keys = 2 bytes
+        if key in ("\x00", "\xe0"):
+            msvcrt.getwch() #eat 2nd byte ignored
+            return ""
+        return key
+else:
+    import tty, termios
+    def getKey():
+        # read inputs as raw keypress ie hold w keep forward let go = stop
+        fd = sys.stdin.fileno()
+        old = termios.tcgetattr(fd)
+
+        try:
+            tty.setraw(fd)
+            return sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old)
 
 def main():
 
@@ -10,15 +38,16 @@ def main():
     parser.add_argument("--speed", type=int, default=30, help="Drive speed 1-63 (default: 30)")
     args = parser.parse_args()
 
+
     with MotorController(mock=args.mock) as motor:
         drive = Drive(motor, speed= args.speed)
 
         commands = {
-        "w": drive.forward,
-        "s": drive.backward,
-        "a": drive.left,
-        "d": drive.right,
-        " ": drive.stop,  # space
+            "w": drive.forward,
+            "s": drive.backward,
+            "a": drive.left,
+            "d": drive.right,
+            " ": drive.stop,  # space
         }
 
         print("Robot ready")
@@ -28,9 +57,15 @@ def main():
         try:   
             while True:
 
-                cmd = input("> ").lower()
-                if cmd == "q":
+                cmd = getKey()
+
+                #quit handles [\x03 = ctl+c], [\1xb = esc]
+                if cmd in ("q", "\x03", "\x1b"):
+                    print("\nQuitting...")
+                    drive.stop()
+                    print("Done!")
                     break
+
                 elif cmd == "+":
                     drive.setSpeed(min(Drive.MAX_SPEED, drive.speed + 5))
             
@@ -41,9 +76,11 @@ def main():
                     commands[cmd]()
             
                 else:
-                    print("Unknown command")
+                    #silent ignore raw unknown inp
+                    pass
         except KeyboardInterrupt:
             print("\nInterrupted")
+            drive.stop()
 
 if __name__ == "__main__":
     main()
